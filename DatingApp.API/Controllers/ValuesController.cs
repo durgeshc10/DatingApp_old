e3ef9f3350370b755a8e8bb97a8829c8035b7188
Durgesh Chauhan <durgeshc10@gmail.com>
2018-11-08 01:12:01 +0530

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatinApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
