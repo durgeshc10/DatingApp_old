6fee63ad80c240de5e7f1e9298f7b5f7e932bfb0
Durgesh Chauhan <durgeshc10@gmail.com>
2018-11-15 02:36:50 +0530

    End of Secion 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-import { NEXT } from '@angular/core/src/render3/interfaces/view';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,27 +10,28 @@ import { NEXT } from '@angular/core/src/render3/interfaces/view';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in Success');
+      this.alertify.success('Logged in Success');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
+    /* const token = localStorage.getItem('token');
+    return !!token; */
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('Logged Out');
   }
 
 }
